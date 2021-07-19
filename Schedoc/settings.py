5fef1eeb2d228694f7aa9e31231fc81a54723593
Vishwas <vishwaspuri09@gmail.com>
2021-07-20 00:37:48 +0530

    Changed DB configuration

--- Schedoc/settings.py
+++ Schedoc/settings.py
@@ -88,12 +88,24 @@ LOGIN_REDIRECT_URL = "/home"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'schedoc',
+            'USER': 'admin',
+            'PASSWORD': 'schedoc',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
