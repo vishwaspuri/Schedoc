812b4b39491ada912fa70a6877dce318736d84a4
Vishwas <vishwaspuri09@gmail.com>
2021-07-20 11:16:04 +0530

    changed api key

--- appointment/utils/send_email.py
+++ appointment/utils/send_email.py
@@ -12,7 +12,7 @@ def send_mail(otp, email):
     )
 
     try:
-        sg = SendGridAPIClient('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')
+        sg = SendGridAPIClient('')
         response = sg.send(message)
         return True, response.status_code
     except Exception as e:
